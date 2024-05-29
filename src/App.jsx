513c38eb54088a1963241a2e097afaf1e85da710
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-29 07:06:35 -0400

    jobsInfo + Duties setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
-const url = 'https://www.course-api.com/react-tabs-project';
+import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
+
+const url = "https://www.course-api.com/react-tabs-project";
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="jobs-center">
+      {/* button container */}
+      {/* job info */}
+      <JobInfo jobs={jobs} />
+    </section>
+  );
 };
 export default App;
---
+++ src/JobInfo.jsx
@@ -0,0 +1,14 @@
+import Duties from "./Duties";
+
+const JobInfo = ({ jobs }) => {
+  const { title, dates, duties, company } = jobs[0];
+  return (
+    <article className="job-info">
+      <h3>{title}</h3>
+      <span className="job-company">{company}</span>
+      <p className="job-date">{dates}</p>
+      <Duties duties={duties} />
+    </article>
+  );
+};
+export default JobInfo;
