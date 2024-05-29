bd8e54f88320ecba3f807e2e4a388e93821cf4be
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-29 17:09:35 -0400

    duties component

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,5 +1,18 @@
-const Duties = (duties) => {
-  console.log(duties);
-  return <div>Duties</div>;
+import { FaAngleDoubleRight } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
+const Duties = ({ duties }) => {
+  return (
+    <div>
+      {duties.map((duty, index) => {
+        const id = uuidv4();
+        return (
+          <div key={id} className="job-desc">
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default Duties;
